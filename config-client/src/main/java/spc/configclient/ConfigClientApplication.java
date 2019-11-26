b506c24154520f7d270c29c028548e9165d42499
LittleWoodCover <772126311@qq.com>
2019-11-26 10:22:18 +0800

    config

--- config-client/src/main/java/spc/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/spc/configclient/ConfigClientApplication.java
@@ -3,9 +3,14 @@ package spc.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
+@RestController
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
